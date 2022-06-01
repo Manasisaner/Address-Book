95ef998b991041f10cc575780a9f9decc90f3449
snehasonwane <snehasonwane01@gmail.com>
2022-06-01 22:19:16 +0530

    [Manasi]Add :Add data

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -14,4 +14,75 @@ public  class AddressBook
 
 
     }
+    public void AddContact()
+    {
+
+        Console.WriteLine("Enter the Address Details");
+        Console.WriteLine("1-FirstName 2-LastName 3-Address 4-City 5-State 6-Zip 7-PhoneNumber 8-Email");
+
+        Contact contact1 = new Contact()
+        {
+
+            FirstName = Console.ReadLine(),
+
+            LastName = Console.ReadLine(),
+
+            Address = Console.ReadLine(),
+
+            City = Console.ReadLine(),
+
+            State = Console.ReadLine(),
+
+            Zip = Convert.ToInt32(Console.ReadLine()),
+
+            PhoneNumber = Convert.ToInt64(Console.ReadLine()),
+
+            Email = Console.ReadLine(),
+
+        };
+
+
+        Contact contact2 = new Contact()
+        {
+            FirstName = "Tomeshwar",
+
+            LastName = "Sahu",
+
+            Address = "Bhilai",
+
+            City = "Mysuru",
+
+            State = "Banglore",
+
+            Zip = 490020,
+
+            PhoneNumber = 82828883837,
+
+            Email = "tomeshwar@com",
+
+        };
+
+        Contact contact3 = new Contact()
+        {
+            FirstName = "Govind",
+
+            LastName = "Bhatt",
+
+            Address = "Bhilai",
+
+            City = "Mumbai",
+
+            State = "Maharasta",
+
+            Zip = 490021,
+
+            PhoneNumber = 82828883837,
+
+            Email = "govind@gmail.com",
+
+        };
+        addContact.Add(contact1);
+        addContact.Add(contact2);
+        addContact.Add(contact3);
+    }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,2 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using AddressBook;
+class program
+{
+
+    public static void Main(String[] args)
+    {
+        AddressBook contact = new AddressBook();
+
+        Console.WriteLine("Welcome to Address Book Project");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("1.Add Contact\n2.Display Contact\n3.Update Contact\n4.Delete Contact\n5.Add New Contact\n6.Add Dictionery\n7.Display Dictionary\n8.Search By City & State\n9.Sorting IN Alfabetical\n10.Sorting According ToCity");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    contact.AddContact();
+                    break;
+                case 2:
+                    contact.Display();
+                    break;
+                case 3:
+                    contact.Update();
+                    break;
+                case 4:
+                    contact.Delete();
+                    break;
+                case 5:
+                    contact.AddDetails();
+                    break;
+                case 6:
+                    Console.WriteLine("Enter FirstName ");
+                    string name = Console.ReadLine();
+                    contact.AddDictionary(name);
+                    break;
+                case 7:
+                    Console.WriteLine("Enter Name to display data in dictionary: ");
+                    string dictionary = Console.ReadLine();
+                    contact.DisplayDictionary(dictionary);
+                    break;
+
+                case 8:
+                    contact.SearchByCityState();
+                    break;
+
+                case 9:
+                    contact.SortingDataByPersonName();
+                    break;
+                case 10:
+                    contact.SortingDataByCityStateOrZip();
+                    break;
+
+                case 11:
+                    contact.sorting();
+                    break;
+
+                case 0:
+                    check = false;
+                    break;
+
+            }
+        }
+
+
+    }
+}
